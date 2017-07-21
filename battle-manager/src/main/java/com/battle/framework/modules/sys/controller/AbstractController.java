a7f890eb9d20e4ec49fb1d46dc7773c1ec69c5db
zhangbing <zhangbingbeyondbit@163.com>
2017-07-21 17:29:24 +0800

    one day

---
+++ battle-manager/src/main/java/com/battle/framework/modules/sys/controller/AbstractController.java
@@ -0,0 +1,22 @@
+package com.battle.framework.modules.sys.controller;
+
+import com.battle.framework.modules.sys.domain.SysUser;
+import org.apache.shiro.SecurityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Controller公共组件
+ * Created by Soldier.Cheung on2017/07/20
+ */
+public abstract class AbstractController {
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+	
+	protected SysUser getUser() {
+		return (SysUser) SecurityUtils.getSubject().getPrincipal();
+	}
+
+	protected Long getUserId() {
+		return getUser().getUserId();
+	}
+}
